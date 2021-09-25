a49d285684b6d0ded24ff8e3f3d627b2b27a6691
Adam Kotkowski <adam.kotkowski@gmail.com>
2021-09-25 20:18:41 +0200

    init spring boot project

---
+++ src/main/kotlin/com/akotkowski/simpledatawarehouse/SimpleDataWarehouseApplication.kt
@@ -0,0 +1,11 @@
+package com.akotkowski.simpledatawarehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleDataWarehouseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleDataWarehouseApplication>(*args)
+}
---
+++ src/test/kotlin/com/akotkowski/simpledatawarehouse/SimpleDataWarehouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akotkowski.simpledatawarehouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleDataWarehouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
